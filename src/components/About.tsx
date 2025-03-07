b0d65c71154564be43c5b759e6b1633d22a04116
Geoo Chi <geoochi@geoochi.com>
2025-03-07 17:03:06 +0800

    update

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,10 +1,18 @@
 import { Link } from 'react-router-dom'
+import { useState, useEffect } from 'react'
 
 const About: React.FC = () => {
+  const [count, setCount] = useState(0)
+  useEffect(() => {
+    fetch('/api/get-count')
+      .then(res => res.json())
+      .then(data => setCount(data.count))
+  }, [])
   return (
     <div className='flex flex-col h-screen items-center justify-center gap-4'>
       <p className='text-4xl'>About Page</p>
       <Link to='/'>← Home Page</Link>
+      <p>server count: {count}</p>
     </div>
   )
 }
